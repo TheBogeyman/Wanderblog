262f2d134f6b0827d8e5052cd65af13f0289474c
TheBogeyman <1305795@rgu.ac.uk>
2015-12-16 03:18:16 +0000

    Populate Table test from adventureForm

--- routes/adventureForm.js
+++ routes/adventureForm.js
@@ -21,6 +21,12 @@ module.exports = function (app,db) {
 
 
             db.getConnection(function (err, connection) {
+                connection.queryRaw("CREATE TABLE asdf", function (err, results){
+                    if (err) throw err;
+
+                });
+
+
                 connection.query('insert into adventure set ?', adventure, function (err, result) {
                     //catch mysql connection error
                     if (err) throw err;
